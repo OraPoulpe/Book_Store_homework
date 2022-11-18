21bdb6be987506a6cc0e061d7540d569f38f1b30
Sergey <ssazonov69@gmail.com>
2022-11-18 23:14:21 +0500

    add_router

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,41 @@ import Layout from "./components/Layout/Layout";
 import { MainPage } from "./pages/MainPage/MainPage";
 import { mock } from "./constants/mock";
 import BookPage from "./pages/BookPage/BookPage";
-import {Routes,Route,BrowserRouter,Navigate} from "react-router-dom"
+import { Link, Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 
+import styles from "./styleApp.module.css"
 
-export function App(){
-
-    return <Layout>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/Book_Store_homework" element={<MainPage genres={ mock }/>}/>
-                <Route path="/Book_Store_homework/book" element={<BookPage book={mock[0].books[1]}/>}/>
-                {/* <Route
-                path="*"
-                element={<Navigate to="/" replace />}
-                /> */}
-            </Routes>
-        </BrowserRouter>
+export function App() {
+  return (
+    <Layout>
+      <BrowserRouter>
+        <header className={styles.header}>
+          <p className={styles.store_text}>Магазин</p>
+          <div className={styles.nav_bar}>
+              <Link className={styles.btn_book_page} to="/Book_Store_homework">
+                Главная страница
+              </Link>
+              <Link className={styles.btn_book_page} to="/Book_Store_homework/book" >
+                Страница книги
+              </Link>
+          </div>
+          <button className={styles.btn_basket} />
+        </header>
+        <Routes>
+          <Route
+            path="/Book_Store_homework"
+            element={<MainPage genres={mock} />}
+          />
+          <Route
+            path="/Book_Store_homework/book"
+            element={<BookPage book={mock[0].books[1]} />}
+          />
+          <Route
+            path="*"
+            element={<Navigate to="/Book_Store_homework" replace />}
+          />
+        </Routes>
+      </BrowserRouter>
     </Layout>
+  );
 }
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,21 +1,13 @@
-import React from 'react'
-import styles from './styleLayout.module.css'
-
+import React from "react";
+import styles from "./styleLayout.module.css";
+import { Link, BrowserRouter } from "react-router-dom";
 
 const Layout = (props) => {
-    return <div className={styles.main_layout}>
-        <header className={styles.header}>
-            <p className={styles.store_text}>Магазин</p>
-            <div className={styles.nav_bar}>
-                <a className={styles.btn_book_page} href='/Book_Store_homework'>Главная страница</a>
-                <a className={styles.btn_book_page} href='/Book_Store_homework/book'>Страница книги</a>
-            </div>
-            <button className={styles.btn_basket}/>
-        </header>
-        <div className={styles.content_layout}>
-            { props.children }
-        </div>
+  return (
+    <div className={styles.main_layout}>
+      <div className={styles.content_layout}>{props.children}</div>
     </div>
-}
+  );
+};
 
-export default Layout
+export default Layout;
